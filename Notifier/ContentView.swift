7281a902339bab42382ab81c69e492b76fc8f8ef
XueshiQiao <qiaoxueshi@gmail.com>
2026-02-16 01:26:25 +0800

    Initial Commit

---
+++ Notifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Notifier
+//
+//  Created by Xueshi Qiao on 2/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Notifier/NotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotifierApp.swift
+//  Notifier
+//
+//  Created by Xueshi Qiao on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct NotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
